512b9ea19d917c1e71e3f7c952f61782bf86378f
Przemek Rostek <rochu9900@gmail.com>
2024-04-26 10:13:41 +0200

    Kolejne modyfikacje

--- ablog/ablog/urls.py
+++ ablog/ablog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theblog.urls')),
     path('members/', include('django.contrib.auth.urls')),
-    path('', include('members.urls'))
+    path('members/', include('members.urls'))
 ]

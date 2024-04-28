5374043bc4cdd877d4397fe5cfdcf968fd96108a
Przemek Rostek <rochu9900@gmail.com>
2024-04-28 09:54:42 +0200

    Dalsze usprawnienia

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
 
 # from . import views
-from .views import ArticleDetailView, HomeViev, AddPostView, UpdatePostView, DeletePostView, AddCategoryView
+from .views import (
+    AddCategoryView,
+    AddPostView,
+    ArticleDetailView,
+    CategoryView,
+    DeletePostView,
+    HomeViev,
+    UpdatePostView,
+)
 
 urlpatterns = [
     # path("", views.home, name="home")
     path("", HomeViev.as_view(), name="home"),
     path("article/<int:pk>", ArticleDetailView.as_view(), name="article-detail"),
-    path("add_post/", AddPostView.as_view(), name='add_post'),
-    path("article/edit/<int:pk>", UpdatePostView.as_view(), name='update-post'),
-    path("article/<int:pk>/remove", DeletePostView.as_view(), name='delete-post'),
-    path("add_category/", AddCategoryView.as_view(), name='add_category'),
+    path("add_post/", AddPostView.as_view(), name="add_post"),
+    path("article/edit/<int:pk>", UpdatePostView.as_view(), name="update-post"),
+    path("article/<int:pk>/remove", DeletePostView.as_view(), name="delete-post"),
+    path("add_category/", AddCategoryView.as_view(), name="add_category"),
+    path("category/<str:cats>/", CategoryView, name="category"),
 ]
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-from .models import Post, Category
-from .forms import PostForm, EditForm
-
+from .forms import EditForm, PostForm
+from .models import Category, Post
 
 # def home(request):
 #     return render(request, 'home.html', {})
@@ -13,39 +18,40 @@ from .forms import PostForm, EditForm
 class HomeViev(ListView):
     model = Post
     template_name = "home.html"
-    ordering = ['-post_date']
+    ordering = ["-post_date"]
     # ordering = ['-id']
 
+def CategoryView(request, cats):
+    return render(request, 'categories.html', {'cats': cats})
 
 
 class ArticleDetailView(DetailView):
     model = Post
-    template_name = 'article_details.html'
+    template_name = "article_details.html"
 
 
 class AddPostView(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'add_post.html'
+    template_name = "add_post.html"
     # fields = '__all__'
     # fields = ('body', 'title')
 
+
 class AddCategoryView(CreateView):
     model = Category
-    template_name = 'add_category.html'
-    fields = '__all__'
-    
+    template_name = "add_category.html"
+    fields = "__all__"
+
 
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
-    template_name = 'update_post.html'
+    template_name = "update_post.html"
     # fields = ['title', 'title_tag', 'body']
+
 
 class DeletePostView(DeleteView):
     model = Post
-    template_name = 'delete_post.html'
-    success_url = reverse_lazy('home')
-
-
-
+    template_name = "delete_post.html"
+    success_url = reverse_lazy("home")

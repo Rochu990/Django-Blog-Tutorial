4a636323d9b7c7f95071101b88e67b0e3d498ddf
Przemek Rostek <rochu9900@gmail.com>
2024-05-23 06:39:46 +0200

    Zmiany w formularzu rejestracyjnym nowych uzytkownikow

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -2,9 +2,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import SingUpForm
 
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SingUpForm
     template_name = "registration/register.html"
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("login")

fc6e1752f67b9cf70010f3ce1925bcc0d4d25ddd
Przemek Rostek <rochu9900@gmail.com>
2024-05-27 08:40:47 +0200

    Wpowradzono okno edycji tekstu bloga przy pomocy ckeditor

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -3,6 +3,7 @@ from datetime import date, datetime
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
@@ -19,7 +20,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    # body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default="coding")
     likes = models.ManyToManyField(User, related_name="blog_post")

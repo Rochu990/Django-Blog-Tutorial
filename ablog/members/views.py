e75d85dbfcb7a599f1dc15db68e23c1a651d5213
Przemek Rostek <rochu9900@gmail.com>
2024-05-27 11:40:23 +0200

    Dodano możlwiość edytowania profilu

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -9,3 +9,11 @@ class UserRegisterView(generic.CreateView):
     form_class = SingUpForm
     template_name = "registration/register.html"
     success_url = reverse_lazy("login")
+
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = "registration/edit_profile.html"
+    success_url = reverse_lazy("home")
+
+    def get_object(self):
+        return self.request.user

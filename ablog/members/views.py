8bc7f61792e2e1c3f774574f9c2429f25d5e4d7c
Przemek Rostek <rochu9900@gmail.com>
2024-05-28 12:16:53 +0200

    Zmodyfikowano formularz edycji profilu uzytkownika

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import SingUpForm
+from .forms import SingUpForm, EditProfileForm
 
 
 class UserRegisterView(generic.CreateView):
@@ -11,7 +11,7 @@ class UserRegisterView(generic.CreateView):
     success_url = reverse_lazy("login")
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = "registration/edit_profile.html"
     success_url = reverse_lazy("home")
 

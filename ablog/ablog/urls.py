2904d2da079009afe7f16755a06396dacb6973f3
Przemek Rostek <rochu9900@gmail.com>
2024-05-31 21:07:34 +0200

    Dodano mozliwosc przeslania zdjecia naglowkowego posta

--- ablog/ablog/urls.py
+++ ablog/ablog/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('theblog.urls')),
-    path('members/', include('django.contrib.auth.urls')),
-    path('members/', include('members.urls')),
-    path('<int:uid>/', include('members.urls')),
-]
+    path("admin/", admin.site.urls),
+    path("", include("theblog.urls")),
+    path("members/", include("django.contrib.auth.urls")),
+    path("members/", include("members.urls")),
+    path("<int:uid>/", include("members.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
